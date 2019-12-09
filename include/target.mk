631cf4be4486d4868003019b7a328a725b09c9a8
Lienol <lawlienol@gmail.com>
2019-12-09 22:59:52 +0800

    add default-settings package

--- include/target.mk
+++ include/target.mk
@@ -13,7 +13,8 @@ __target_inc=1
 DEVICE_TYPE?=router
 
 # Default packages - the really basic set
-DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd urandom-seed urngd luci luci-compat luci-lib-ipkg
+DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd urandom-seed urngd luci luci-compat luci-lib-ipkg \
+default-settings
 # For nas targets
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
 # For router targets
